50505e9830b164150c280f20a51b90fdfe54299e
Jeremy Brensinger <jbrensinger03@gmail.com>
2025-08-03 00:00:54 -0400

    Add object data to represent individual carousel items

---
+++ data/carousel-items.js
@@ -0,0 +1,23 @@
+carouselItems = [
+    {
+        heading: 'Recent Releases',
+        description: 'Browse all of our recent releases!',
+        linkText: 'View Recent Releases',
+        linkHref: '',
+        imagePath: ''
+    },
+    {
+        heading: 'Popular Releases',
+        description: 'Browse all of our popular releases!',
+        linkText: 'View Recent Releases',
+        linkHref: '',
+        imagePath: ''
+    },
+    {
+        heading: 'All Releases',
+        description: 'Browse all of our current releases!',
+        linkText: 'View All Releases',
+        linkHref: '',
+        imagePath: ''
+    },
+];
