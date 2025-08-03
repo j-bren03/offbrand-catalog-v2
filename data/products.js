5ea3767f9824db6e4bbc8e57c6ae50ba982b5023
Jeremy Brensinger <jbrensinger03@gmail.com>
2025-08-03 14:26:02 -0400

    Create a simple model for product data

---
+++ data/products.js
@@ -0,0 +1,22 @@
+const products = [
+    {
+        productId: 0,
+        productName: 'Product 1',
+        productPriceCents: '0',
+        productDescription: '',
+        productPhotographer: '',
+        productSizes: [],
+        productsTags: [],
+        productImages: []
+    },
+    {
+        productId: 1,
+        productName: 'Product 2',
+        productPriceCents: '0',
+        productDescription: '',
+        productPhotographer: '',
+        productSizes: [],
+        productsTags: [],
+        productImages: []
+    }
+];
